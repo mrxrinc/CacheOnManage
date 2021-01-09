15028c747b132f543940bf3d902c9945e8add224
mohammad <mrxmirzaei@gmail.com>
2021-01-09 11:15:37 +0330

    remove warning boxes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import AppNavigator from "./navigation";
+import { LogBox } from "react-native";
 import { Provider } from "react-redux";
 // import { store } from "./redux/store";
 import "./utils/localize";
 import { codePushify } from "./utils/codepush";
 // Redux
 import appStore from "./store";
+
+LogBox.ignoreAllLogs();
 
 const globalAny: any = global;
 // for inspecting network requests in debugger
