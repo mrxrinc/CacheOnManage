7b21f7b1b94c65c6a7eaa619511978a697781f31
mohammad <mrxmirzaei@gmail.com>
2021-01-25 16:07:50 +0330

    feat(update): get device info

--- src/pages/update/index.tsx
+++ src/pages/update/index.tsx
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Platform, Text } from "react-native";
 import Force from "pages/update/force";
 import ModalUpdate from "./modal";
+import DeviceInfo from "react-native-device-info";
+
 const index = () => {
-  const [status, setStatus] = useState("force");
+  const [status, setStatus] = useState("updated");
+  useEffect(() => {
+    let uniqueId = DeviceInfo.getUniqueID();
+    let version = DeviceInfo.getVersion();
+    let systemVersion = DeviceInfo.getSystemVersion();
+
+    console.log("Version: " + version);
+    console.log("Unique id: " + uniqueId);
+    console.log("OS version: " + systemVersion);
+    console.log("OS: " + Platform.OS);
+  }, []);
   switch (status) {
     case "updated":
       return null;
