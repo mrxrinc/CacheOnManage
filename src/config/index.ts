5fcccf3422283e3013556834b2e778c523091758
mohammad <mrxmirzaei@gmail.com>
2021-03-05 18:11:16 +0330

    fix: change url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-//export const BASE_URL = "http://91.245.228.78:8080";
+export const BASE_URL = "http://91.245.228.78:8080";
 //export const BASE_URL = "http://192.168.2.11:8080"; // farzad ip
-export const BASE_URL = "http://172.16.0.197:8081"; // zahra ip
+// export const BASE_URL = "http://172.16.0.197:8081"; // zahra ip
 
 export const AXIOS_LOG_REQUEST = false;
 export const AXIOS_LOG_RESPONSE = true;
