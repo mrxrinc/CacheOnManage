8c244d6aa4db11c1cd72703069b54905e175c5c7
mohammad <mrxmirzaei@gmail.com>
2021-03-15 12:40:39 +0330

    fix: the logger on production fixed to prevent crashes

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -5,7 +5,7 @@ import AsyncStorage from "@react-native-community/async-storage";
 if (__DEV__) {
   logger = Reactotron.log;
 } else {
-  logger = null;
+  logger = () => null;
 }
 
 const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
